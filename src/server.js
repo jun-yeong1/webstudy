1e9a41d41eb9021ddde5a295a2e5bb47721f0810
jun-yeong1 <dlwnsdud1320@gmail.com>
2022-12-03 19:36:26 +0900

    3.4 connection
    채팅할 때 했던 유저 연결

--- src/server.js
+++ src/server.js
@@ -14,6 +14,14 @@ app.get("/*", (_, res) => res.redirect("/"));
 const httpServer = http.createServer(app);
 const wsServer = new Server(httpServer);
 
+wsServer.on("connection", (socket) => {
+  socket.on("join_room", (roomName, done) => {
+    socket.join(roomName);
+    done();
+    socket.to(roomName).emit("welcome");
+  });
+});
+
 httpServer.listen(3000, handleListen);
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
